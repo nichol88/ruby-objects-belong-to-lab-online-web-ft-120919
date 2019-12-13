125f48f59f0666ccfb0f18ae86d122b47b2bff07
Brian Nicholls <nichol88@gmail.com>
2019-12-13 06:50:46 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,6 @@
+class Post
+  attr_accessor :title, :author
+  def initialize(title = nil)
+    @title = title
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,6 @@
+class Song
+  attr_accessor :title, :artist
+  def initialize(title = nil)
+    @title = title
+  end
+end
